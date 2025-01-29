3e74f2da6be84fa5b76676333cd0c45eeb6a887b
Joaquin Arino <dev.joaquinarinog@gmail.com>
2025-01-29 09:22:22 +0100

    update personal info

--- config/education.js
+++ config/education.js
@@ -3,21 +3,19 @@ const education = [
         institution: "KU Leuven",
         degree: "Master of Artificial Intelligence in Business and Industry",
         period: "Sep 2023 - Sep 2024",
-        description: "Grade: Cum Laude\n• Key courses: Machine Learning, Computer Vision, Deep Learning, Business Analytics, Natural Language Processing.\n• Project: Implementation of an online store with computer vision that allows product search by their features with 91% accuracy. Frontend development and communication with the backend deployed on AWS using Docker and GitHub Actions for automation.\n• Project: Development of a computer vision system capable of identifying watch features with over 90% accuracy and predicting their price through a regression model.",
-        skills: ["TensorFlow", "PyTorch"]
+        description: "Grade: Cum Laude",
     },
     {
         institution: "Technological University Dublin",
-        degree: "Computing (Information Technology)",
+        degree: "Erasmus",
         period: "Sep 2021 - Jun 2022",
-        description: "Studies in Information Technology focusing on advanced computing concepts and practical applications."
+        description: ""
     },
     {
-        institution: "Universidad de Zaragoza",
+        institution: "University of Zaragoza",
         degree: "Degree in Computer Engineering",
-        period: "- Jun 2023",
-        description: "Computer Engineering degree with focus on software development and systems architecture.",
-        skills: ["C++", "Python", "MySQL"]
+        period: "Jun 2023",
+        description: "",
     }
 ];
 
